ffcbe4fe9dadfbb7d0fa8f38eebdce3f847ced80
Bob <731653765@qq.com>
2022-10-18 22:33:18 +0800

    Time: 101 ms (33.95%), Space: 42.4 MB (28.60%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -11,18 +11,14 @@
  * @return {number[]}
  */
 var postorderTraversal = function(root) {
-    // it is preorder traversal, just reverse it
     if(!root) return []
     let result = []
-    // push stack left => right pop stack right => left
-    // result mid => right => left
-    // at the end just reverse the result array
     let stack = [root]
     while(stack.length) {
-       let node  = stack.pop()
-            result.push(node.val);
-            node.left && stack.push(node.left)
-            node.right && stack.push(node.right) 
+        let node  = stack.pop()
+        result.unshift(node.val);
+        node.left && stack.push(node.left)
+        node.right && stack.push(node.right) 
     }
-    return result.reverse()
+    return result
 };
