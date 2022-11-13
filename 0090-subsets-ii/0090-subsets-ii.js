dcaa27874abb12843b68b76c31bb1ad469e4b63d
Bob <731653765@qq.com>
2022-11-13 15:59:51 +0800

    Time: 71 ms (95.46%), Space: 44.1 MB (90.33%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    let result = []
+    let path = []
+    nums.sort((a,b) => a - b)
+    let len = nums.length
+    
+    const backTracking = (startIndex) => {
+        result.push([...path])
+        
+        for(let i = startIndex; i < len; i++ ) {
+            if(i > startIndex && nums[i] === nums[i - 1]) continue
+            path.push(nums[i])
+            backTracking(i + 1)
+            path.pop()
+        }
+    }
+    backTracking(0)
+    return result
+};
