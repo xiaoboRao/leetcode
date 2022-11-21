e4962fa0766e69b34d9c71de831efb8fd263a583
Bob <731653765@qq.com>
2022-11-21 21:25:37 +0800

    Time: 91 ms (53.80%), Space: 41.7 MB (77.69%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+    if(n <= 3) return n
+    let dp = [0, 1, 2, 3]
+    
+    for(let i = 3; i <= n ; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+};
