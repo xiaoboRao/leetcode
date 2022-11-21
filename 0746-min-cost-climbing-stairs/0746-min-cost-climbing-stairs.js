66fad0bd9256eacb69ecc5bfa8f54b17018f6303
Bob <731653765@qq.com>
2022-11-21 21:53:26 +0800

    Time: 96 ms (69.20%), Space: 46.6 MB (5.38%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let dp = [0, 0]
+    for(let i = 2; i <= cost.length; i++) {
+        
+        dp[i] = Math.min((dp[i - 1] + cost[i - 1]), (dp[i - 2] + cost[i - 2]))
+    }
+    console.log(dp)
+    return dp[dp.length - 1]
+};
