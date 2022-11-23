06874ab9bd7b296175873d162e4e4fad5865fea7
Bob <731653765@qq.com>
2022-11-23 20:50:07 +0800

    Time: 105 ms (32.67%), Space: 46.8 MB (5.01%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const dp = Array(m).fill().map(item => Array(n))
+            console.log(dp)
+
+    for(let i = 0; i < m; i++){
+        dp[i][0] = 1 
+    } 
+    for(let j = 0; j < n; j++) {
+        dp[0][j] = 1  
+    } 
+        console.log(dp)
+    
+    for(let i = 1; i< m; i++) {
+        for(let j = 1; j<n; j++) {
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        }
+    }
+    
+    return dp[m - 1][n - 1]
+};
