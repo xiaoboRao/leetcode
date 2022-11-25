eac57337c1ce3c66d3c512a24bd5cd73e966ffd3
Bob <731653765@qq.com>
2022-11-25 16:33:45 +0800

    Time: 82 ms (72.97%), Space: 41.2 MB (99.02%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.js
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.js
@@ -6,14 +6,10 @@ var numTrees = function(n) {
     let dp = new Array(n+1).fill(0);
     dp[0] = 1;
     dp[1] = 1;
-
-
     for(let i = 2; i <=n; i++) {
         for(let j = 1; j <= i ; j++) {
             dp[i] += dp[j - 1]*dp[i - j]
         }
     }
-    console.log(dp)
     return dp[n];
-    
 };
