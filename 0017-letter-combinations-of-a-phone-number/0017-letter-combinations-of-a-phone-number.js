b3799c95087786bfcf392607431d063130ea035f
Bob <731653765@qq.com>
2022-11-29 22:33:33 +0800

    Time: 71 ms (79.92%), Space: 41.8 MB (74.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -12,20 +12,24 @@ var letterCombinations = function(digits) {
     let path = []
     
     const map = ["", "", "abc", "def", "ghi", "jkl", "mno", "qprs", "tuv", "wxyz"]
-    const backtracking = (digits, length, startIndex) => {
+    const backtracking = (startIndex) => {
+        
         if(path.length === length) {
-            result.push(path.join(''))
+            let str = ''
+            path.forEach( item => {
+                str += item
+            })
+            result.push(str)
             return
         }
-        console.log(map[digits[startIndex]])
-        for(const value of map[digits[startIndex]]) {
-            path.push(value)
-            backtracking(digits, length, startIndex + 1)
+        for(const val of map[parseInt(digits[startIndex])]) {
+            path.push(val)
+            backtracking(startIndex + 1)
             path.pop()
         }
     }
     
-    backtracking(digits, length, 0)
+    backtracking( 0)
     return result
     
 };
