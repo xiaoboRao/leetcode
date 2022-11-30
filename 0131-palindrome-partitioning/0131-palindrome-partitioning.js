c74cd67b5275e2da3899d39a402a44faf2c73461
Bob <731653765@qq.com>
2022-11-30 21:24:08 +0800

    Time: 618 ms (5.13%), Space: 80.5 MB (58.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -9,20 +9,17 @@ var partition = function(s) {
     
     let path = []
     let result = []
-    
     let len = s.length
-    
     const isPalindrome = (str) => {
         let strLen = str.length
-        
         for(let i = 0; i< strLen; i++) {
             if(str[i] !== str[--strLen]) return false
         }
         return true
         
     }
-    
     const backTracking = (startIndex) => {
+        // if path is not palindrome arr, then  just return
         let flag = false
         path.forEach( item => {
                 if(!isPalindrome(item)) {
@@ -31,11 +28,12 @@ var partition = function(s) {
             }
         )
         if(flag) return
+        // you should collect the element util end , now path is palindrome arr
         if(startIndex === len) {
             result.push([...path])
         }
         
-        for(let i = startIndex; i<s.length; i++) {
+        for(let i = startIndex; i<len; i++) {
             let str = s.substr(startIndex, i - startIndex + 1);
             path.push(str)
             backTracking(i + 1)
