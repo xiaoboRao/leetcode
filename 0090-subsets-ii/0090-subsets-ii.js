c3741e0c467c6daa46974a5e1174b7f51e66188f
Bob <731653765@qq.com>
2022-12-02 21:14:32 +0800

    Time: 91 ms (79.65%), Space: 44.7 MB (61.79%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -9,10 +9,9 @@ var subsetsWithDup = function(nums) {
     let len = nums.length
     
     const backTracking = (startIndex) => {
-        result.push([...path])
-        
-        for(let i = startIndex; i < len; i++ ) {
-            if(i > startIndex && nums[i] === nums[i - 1]) continue
+       result.push([...path])
+        for(let i = startIndex; i < len; i++) {
+            if(i > startIndex && nums[i - 1] === nums[i]) continue
             path.push(nums[i])
             backTracking(i + 1)
             path.pop()
