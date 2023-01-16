9f58084fc742b90298322d544335155231a447ea
Bob <731653765@qq.com>
2023-01-16 21:12:15 +0800

    Time: 159 ms (47.42%), Space: 47.6 MB (99.26%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,6 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    nums =  nums.map(num => num*num)
-    return nums.sort((a,b) => a - b)
+    let resultArr = []
+    let endIndex = nums.length - 1
+    let startIndex = 0
+    
+    while(endIndex >= startIndex) {
+        let startNum = nums[startIndex]
+        let endNum = nums[endIndex]
+
+        if(startNum * startNum >= endNum * endNum) {
+            resultArr.unshift(startNum * startNum)
+            startIndex++
+        } else {
+             resultArr.unshift(endNum * endNum)
+                endIndex--
+        }
+    }
+    return resultArr
 };
