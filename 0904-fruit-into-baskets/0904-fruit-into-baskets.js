59eeb91b53de99a53bbd6bbf051d906a9f336466
Bob <731653765@qq.com>
2023-01-20 21:48:00 +0800

    Time: 116 ms (91.16%), Space: 52.4 MB (87.98%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -3,27 +3,35 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
+    // type: count
     let map = new Map()
     let result = 0
     let left = 0
     let right = 0 
-    
     while(right < fruits.length) {
-        if(map.size === 2 && !map.get(fruits[right])) {
-            const fruit = map.get(fruits[left]) - 1
+        if(map.size < 2) {
+            map.set(fruits[right], (map.get(fruits[right]) || 0) + 1)
+            right++
+            result = Math.max(right - left, result)
+            continue
+        }
+        if(map.get(fruits[right])) {
+            map.set(fruits[right], (map.get(fruits[right]) || 0) + 1)
+            right++
+            result = Math.max(right - left, result)
+            continue
+         } 
+        if(!map.get(fruits[right])){
+             const fruit = map.get(fruits[left]) - 1
             if(fruit === 0) {
                 map.delete(fruits[left], fruit)
             } else {
                 map.set(fruits[left], fruit)
             }
-               left++
-                continue
+             left++
+            continue
+            
         }
-        
-            map.set(fruits[right], (map.get(fruits[right]) || 0) + 1)
-            right++
-            result = Math.max(right - left, result)
     }
-   
     return result
 };
