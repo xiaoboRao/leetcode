eb6e944d0497a37e43167eae37dacefa98d29a96
Bob <731653765@qq.com>
2023-02-12 18:02:34 +0800

    Time: 186 ms (55.96%), Space: 55.7 MB (58.34%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,37 +4,38 @@
  */
 var threeSum = function(nums) {
     nums.sort( (a, b) => a - b)
-    console.log(nums)
     let result = []
     
-    let end = nums.length
-    
-    for(let i = 0 ; i < end; i++) {
-        if(nums[i] > 0) return result
-        let left = i + 1
-        let right = end - 1
-        if(i > 0 && nums[i] === nums[i - 1]) continue
-        while(left < right) {
-            let leftNum = nums[left]
-            let rightNum = nums[right]
-            let iNum = nums[i]
-            if(leftNum + rightNum + iNum > 0)  {
-                right--
-            } else if(leftNum + rightNum + iNum < 0)  {
-                left++
-            } else {
-                result.push([leftNum, rightNum, iNum ])
-                while(left < right && nums[left] == nums[left + 1]){
-                    left++
-                }
-                while(left < right && nums[right] == nums[right - 1]){
-                    right--
-                }
-                left++
-                right--
+    for(let i = 0; i < nums.length; i++) {
+        
+        if(nums[0] > 0) return []
+        
+        if( i > 0 && nums[i] === nums[i - 1]) continue
+        
+        let movePoint = i + 1
+        let end = nums.length - 1
+        while(movePoint < end) {
+            
+            let moveElement =  nums[movePoint]
+            let lastElement = nums[end]
+            let sum = nums[i] + moveElement + lastElement
+            
+            if(sum > 0) {
+                end--
             }
-        }   
+            if(sum === 0)   {
+               result.push([nums[i], moveElement, lastElement]) 
+                movePoint++ 
+                while(nums[movePoint] === nums[movePoint - 1]) {
+                    movePoint++
+                }
+                
+            } 
+            if(sum < 0) {
+                movePoint++
+            }
+        }
     }
-    
+    
     return result
 };
