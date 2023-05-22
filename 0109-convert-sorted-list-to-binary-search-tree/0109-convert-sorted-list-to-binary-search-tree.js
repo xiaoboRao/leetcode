28fdff8278af8e1e2e93c68aa799b8ae72f8a6f6
Bob <731653765@qq.com>
2023-05-22 22:19:39 +0800

    Time: 90 ms (20.53%), Space: 48 MB (28.95%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.js
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.js
@@ -19,23 +19,31 @@
  */
 var sortedListToBST = function(head) {
     let btsArr = []
+    // traverse the list, push the elements to the array
     while(head) {
         btsArr.push(head.val)
         head = head.next
     }
+    // calll constructBST function to build the entire bst 
     return constructBST(btsArr, 0 , btsArr.length - 1)
     
 };
-
+    // write the helper function to construct the BST from Array
 const constructBST = (btsArr, start, end) => {
+    // determine the recursive function ends situation
     if(start > end) return null
-    
+    // calculate the middle elemens of the array
     let midIndex = Math.floor(( start + end )/2)
     let nodeValue = btsArr[midIndex]
+    // build the Tree node  from the middle elements
     let root = new TreeNode(nodeValue)
+    // build the left Tree node left subTrees recursively
     root.left = constructBST(btsArr, start, midIndex - 1)
+        // build the right Tree node left subTrees recursively
+
     root.right = constructBST(btsArr, midIndex + 1, end)
     
+    // return the Tree node
     return root
     
 }
