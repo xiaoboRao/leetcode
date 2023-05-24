654e803687e17dff6df05ddd36afc139d0371ba4
Bob <731653765@qq.com>
2023-05-24 22:37:41 +0800

    Time: 110 ms (15.95%), Space: 52.3 MB (6.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -10,31 +10,57 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    let slow = head
-    let fast = head.next
+    if(!head.next) return head
+    let prev = findPreviousNode(head)
+    let next = prev.next
+    prev.next = null
+
+    let lastHalfPart =  traverseNode(next)
+    let firstHalfPart = head
+    console.log(firstHalfPart, lastHalfPart)
     
-    while(fast && fast.next) {
-        fast = fast.next.next
-        slow = slow.next
+    
+    mergeList(firstHalfPart ,lastHalfPart)
+};
+
+const mergeList = (list1, list2) => {
+    
+    while(list1) {
+       let firstNext = list1.next
+       list1.next = list2
+       list1 = firstNext
+
+        let secondNext = list2.next
+        if(list1) {
+             list2.next = list1
+            list2 = secondNext
+        }
+
     }
+
+}
+
+const traverseNode = (head) => {
     let prev = null
-    let curr = slow.next;
-    slow.next = null;
+    let curr = head
     
     while(curr) {
         let temp = curr.next
         curr.next = prev
         prev = curr
-        curr = temp  
+        curr = temp
     }
-    let first = head
-    let second = prev
-    while(second) {
-        let firstNext = first.next
-        let secondNext = second.next
-        first.next = second
-        second.next = firstNext
-        first = firstNext
-        second = secondNext
-    }
-};
+    return prev
+    
+}
+
+const findPreviousNode = (head) => {
+   let slow =  head
+   let fast = head.next
+   
+   while(fast.next && fast.next.next) {
+         fast = fast.next.next
+        slow = slow.next
+   }
+    return slow
+} 
