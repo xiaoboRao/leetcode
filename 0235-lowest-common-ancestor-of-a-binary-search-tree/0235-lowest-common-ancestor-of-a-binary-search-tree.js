b10e9fa5affdd019230b486d4774dbd0e1bbadbc
Bob <731653765@qq.com>
2023-06-05 21:21:46 +0800

    Time: 93 ms (17.87%), Space: 52.3 MB (50.22%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,24 +13,45 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    // determine the q.val is greater the p.val
-    if(q.val < p.val) {
-        let temp = q
-        q = p
-        p = temp
-        
+  let {node1, node2} =swapNode(p, q)
+  let lcaNode =  dfs(root, node1, node2)
+  return lcaNode
+  // expect(lcaNode.val).toBe(4)
+// expect(lcaNode.val).toBe(6)
+
+};
+
+const dfs = (root, p, q) => {
+    if(!root) return null
+    if(root.val >= p.val && root.val <= q.val) return root
+    
+    let leftNode =  dfs(root.left, p, q)
+    let rightNode = dfs(root.right, p, q)
+    
+    if(leftNode) return leftNode
+    if(rightNode) return rightNode
+    
+    return null
+}
+// swap the node to promise the node1.val is less than node2.val
+const swapNode = (node1, node2) => {
+    if(node1.val > node2. val) {
+        let temp = node2
+        node2 = node1
+        node1 = temp
     }
     
-    const dfs = (root, p, q) => {
-        if(!root) return null
-        if(p.val <=  root.val  &&  root.val <= q.val)  return root
-        let leftNode = dfs(root.left, p, q)
-        let rightNode = dfs(root.right, p, q)
-        
-        if(leftNode) return leftNode
-        if(rightNode) return rightNode
-        return null
-
+    return {node1, node2}
+}
+// test for result
+const expect  = (result) => {
+    return {
+          toBe(value) {
+            if(value !== result) {
+                throw Error(`the result is ${result}, and expected ${value}`)
+            } else {
+                console.log('pass the test')
+            }
+        }
     }
-    return dfs(root, p, q)
-};
+}
