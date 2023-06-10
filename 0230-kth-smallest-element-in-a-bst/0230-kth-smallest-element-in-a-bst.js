87448fbe02277ad516ed105fcf47b1e48100afdd
Bob <731653765@qq.com>
2023-06-10 18:25:05 +0800

    Time: 64 ms (92.95%), Space: 48.5 MB (53.67%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let res = null ;
+    
+    const inOrder = (root) => {
+        
+        if(!root) return null
+        
+        inOrder(root.left)
+        if(res) {
+            return res
+        }
+        
+        k--
+        
+        if(k === 0) {
+           res = root.val
+            return
+        }
+         inOrder(root.right)
+    }
+    inOrder(root)
+    
+   return res
+};
