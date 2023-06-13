ccab696679a46154b59871d090afc27ca55ca090
Bob <731653765@qq.com>
2023-06-13 21:43:43 +0800

    Time: 123 ms (6.04%), Space: 50.8 MB (70.39%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.js
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.js
@@ -12,35 +12,34 @@
  * @return {TreeNode}
  */
 
- function getMinNode(curNode) {
-     while(curNode.left) {
-            curNode = curNode.left
-        }
-        return curNode
- }
+
 var deleteNode = function(root, key) {
-    if(!root) return null
-    const dfs = (node) => {
-        // 1. when key not in the tree
-        if(!node) return null
-        // when node without left and right nodes
-        if(!node.left && !node.right && node.val === key) {
-            return null
-        } else if(!node.left && node.val === key) {
-            // when node just has right node
-            return node.right
-        } else if(!node.right && node.val === key) {
-            // when node just has left node
-            return node.left
-        } else if(node.val === key) {
-            // when node has left and right nodes 
-            const targetNode = getMinNode(node.right)
-            targetNode.left = node.left
-            return node.right
+    
+    const dfs = (root, key) => {
+        if(!root) return null
+        
+        if(root.val < key )  {
+            root.right = dfs(root.right, key)
+        } else if(root.val > key) {
+            root.left = dfs(root.left, key) 
+        } else  {
+            if(!root.right) return root.left
+            if(!root.left) return root.right
+            let val =  findMinNodeValue(root.right)
+            root.val = val
+            root.right = dfs(root.right, val)
+            
         }
-        node.left = dfs(node.left)
-        node.right = dfs(node.right)
-        return node
+        return root
     }
-    return dfs(root)
+    
+    return dfs(root, key)
+ 
 };
+
+const findMinNodeValue = (node) => {
+    while(node.left) {
+        node = node.left
+    }
+    return node.val
+}
